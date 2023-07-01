0e4abf9ccc61e813a5a047c961bbb040825c21a4
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2023-07-01 18:49:44 +0000

    Updating promises using async await

--- src/Utills/location.js
+++ src/Utills/location.js
@@ -1,20 +1,18 @@
-const request=require('request')
-const location=(long,lat,callback)=>{
+const location=async (long,lat)=>{
 
     const url=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=6fb513223d2df4b2b1e5869e588aefe6`
-    request({url:url,json:true},(err,res)=>{
-        if(err){
-            callback('Unable to Connect')
+   
+   try{
+    const response =await fetch(url)
+    const data=await response.json()
+    if(data.code==="404"){
+        throw new Error("Unable to Find Location")
+    }
+    
+    return data.weather[0].description + " It's current temperature " + data.main.temp+ ' degree out. The wind speed is '+data.wind.speed+' m/s at '+data.wind.deg+' degree'
 
-        }else if(res.body.message){
-            callback(res.body.message,undefined)
-
-        }else{
-            const weather=res.body;
-            const data=weather.weather[0].description + " It's current temperature " + weather.main.temp+ ' degree out. The wind speed is '+weather.wind.speed+' m/s at '+weather.wind.deg+' degree'
-            callback(undefined,data)
-
-        }
-    })
+   }catch(error){
+      throw new Error(error.message)
+   }
 }
 module.exports=location
--- src/Utills/weather.js
+++ src/Utills/weather.js
@@ -1,33 +1,27 @@
-const request=require('request');
-const weather=(address,callback)=>{
-    const url=`http://api.openweathermap.org/data/2.5/weather?q=${address}&&appid=6fb513223d2df4b2b1e5869e588aefe6&units=metric`;
-    request({url:url,json:true},(error,response)=>{
-        if(error){
-            callback('Unable to Connect',undefined)
+const request = require('request');
+const weather = async (address) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&&appid=6fb513223d2df4b2b1e5869e588aefe6&units=metric`;
+    try {
 
-        }else if(response.body.message){
-
-            callback('Unable to find place',undefined)
-        }else{
-            const data={
-                lat:response.body.coord.lat,
-                long:response.body.coord.lon,
-                location:response.body.name,
-                minTem:response.body.main.temp_min,
-                maxTem:response.body.main.temp_max,
-                
-
-                
-                
-
-            }
-           
-            callback(undefined,data)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        if(data.cod=="404"){
+            
+            throw Error(data.message)
+        }
+       
+        const arrangeData = {
+            lat: data.coord.lat,
+            long: data.coord.lon,
+            location: data.name,
+            minTem: data.main.temp_min,
+            maxTem: data.main.temp_max,
 
         }
-
-    })
-  
-
+        return arrangeData;
+    }catch (error) {
+        throw  Error(error.message)
 }
-module.exports=weather;
+}
+module.exports = weather;
